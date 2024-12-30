9b58460ba0aca17c475ad21da86ca02997881020
Joko Santosi <ahmadgozali4040@gmail.com>
2024-12-30 12:10:26 +0700

    change svg tag to icon components

--- src/components/TransactionSection/TransactionList.jsx
+++ src/components/TransactionSection/TransactionList.jsx
@@ -1,5 +1,8 @@
 import moneyFormat from "../../utils/moneyFormat"
 import supabaseDelTransaction from "../../api/supabaseDelTransaction"
+import TrashIcon from "../../assets/icon/TrashIcon";
+import TrafficUp from "../../assets/icon/TrafficUp";
+import TrafficDown from "../../assets/icon/TrafficDown";
 
 const TransactionList = ({ transactions, setTransactions }) => {
     const deleteTransaction = async (id) => {
@@ -19,12 +22,12 @@ const TransactionList = ({ transactions, setTransactions }) => {
                     <div className="transaction-icon button" onClick={() => deleteTransaction(transaction.id)}>
                         {transaction.transaction_category === 'income' ?
                             (<div className="transaction-icon-container income">
-                                <svg className='delete-icon' xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><path d="M135.2 17.7L128 32 32 32C14.3 32 0 46.3 0 64S14.3 96 32 96l384 0c17.7 0 32-14.3 32-32s-14.3-32-32-32l-96 0-7.2-14.3C307.4 6.8 296.3 0 284.2 0L163.8 0c-12.1 0-23.2 6.8-28.6 17.7zM416 128L32 128 53.2 467c1.6 25.3 22.6 45 47.9 45l245.8 0c25.3 0 46.3-19.7 47.9-45L416 128z"/></svg>
-                                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512"><path d="M384 160c-17.7 0-32-14.3-32-32s14.3-32 32-32l160 0c17.7 0 32 14.3 32 32l0 160c0 17.7-14.3 32-32 32s-32-14.3-32-32l0-82.7L342.6 374.6c-12.5 12.5-32.8 12.5-45.3 0L192 269.3 54.6 406.6c-12.5 12.5-32.8 12.5-45.3 0s-12.5-32.8 0-45.3l160-160c12.5-12.5 32.8-12.5 45.3 0L320 306.7 466.7 160 384 160z"/></svg>
+                                <TrashIcon className={'delete-icon'} />
+                                <TrafficUp />
                             </div>) :
                             (<div className="transaction-icon-container outcome">
-                                <svg className='delete-icon' xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><path d="M135.2 17.7L128 32 32 32C14.3 32 0 46.3 0 64S14.3 96 32 96l384 0c17.7 0 32-14.3 32-32s-14.3-32-32-32l-96 0-7.2-14.3C307.4 6.8 296.3 0 284.2 0L163.8 0c-12.1 0-23.2 6.8-28.6 17.7zM416 128L32 128 53.2 467c1.6 25.3 22.6 45 47.9 45l245.8 0c25.3 0 46.3-19.7 47.9-45L416 128z"/></svg>
-                                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512"><path d="M384 352c-17.7 0-32 14.3-32 32s14.3 32 32 32l160 0c17.7 0 32-14.3 32-32l0-160c0-17.7-14.3-32-32-32s-32 14.3-32 32l0 82.7L342.6 137.4c-12.5-12.5-32.8-12.5-45.3 0L192 242.7 54.6 105.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3l160 160c12.5 12.5 32.8 12.5 45.3 0L320 205.3 466.7 352 384 352z"/></svg>
+                                <TrashIcon className={'delete-icon'} />
+                                <TrafficDown />
                             </div>)
                         }
                     </div>
--- src/components/UserRecap/UserInoutRecapsPlaceholder.jsx
+++ src/components/UserRecap/UserInoutRecapsPlaceholder.jsx
@@ -1,3 +1,6 @@
+import TrafficDown from "../../assets/icon/TrafficDown";
+import TrafficUp from "../../assets/icon/TrafficUp";
+
 const UserInoutRecapsPlaceholder = () => {
     return (
         <div className="inout-recaps round-15 flex flex-space-between pad-22">
@@ -5,7 +8,7 @@ const UserInoutRecapsPlaceholder = () => {
                 <div className="in">
                     <span className='flex flex-align-center'>
                         Your Income
-                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512"><path d="M384 160c-17.7 0-32-14.3-32-32s14.3-32 32-32l160 0c17.7 0 32 14.3 32 32l0 160c0 17.7-14.3 32-32 32s-32-14.3-32-32l0-82.7L342.6 374.6c-12.5 12.5-32.8 12.5-45.3 0L192 269.3 54.6 406.6c-12.5 12.5-32.8 12.5-45.3 0s-12.5-32.8 0-45.3l160-160c12.5-12.5 32.8-12.5 45.3 0L320 306.7 466.7 160 384 160z"/></svg>
+                        <TrafficUp />
                     </span>
                     <p className='flex'>
                         Loading...
@@ -13,8 +16,8 @@ const UserInoutRecapsPlaceholder = () => {
                 </div>
                 <div className="out">
                     <span className='flex flex-align-center'>
-                        Your Outcome
-                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512"><path d="M384 352c-17.7 0-32 14.3-32 32s14.3 32 32 32l160 0c17.7 0 32-14.3 32-32l0-160c0-17.7-14.3-32-32-32s-32 14.3-32 32l0 82.7L342.6 137.4c-12.5-12.5-32.8-12.5-45.3 0L192 242.7 54.6 105.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3l160 160c12.5 12.5 32.8 12.5 45.3 0L320 205.3 466.7 352 384 352z"/></svg>
+                        Your Expense
+                        <TrafficDown />
                     </span>
                     <p className='flex'>
                         Loading...
