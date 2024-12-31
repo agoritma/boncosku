b5d12f913d75ca3b094b6731a37b5309c1ce6c9f
Joko Santosi <ahmadgozali4040@gmail.com>
2024-12-31 12:56:04 +0700

    adding confirmation box component

---
+++ src/components/ConfirmBox/Index.jsx
@@ -0,0 +1,29 @@
+import './ConfirmBox.css'
+import DangerIcon from '../../assets/icon/DangerIcon'
+import { useEffect } from 'react';
+
+const ConfirmBox = ({ type, message, isConfirmation, onConfirm, onCancel }) => {
+    useEffect(() => {
+        document.body.style.overflow = 'hidden';
+        return () => {
+            document.body.style.overflow = '';
+        };
+    }, []);
+    
+    return (
+        <div className="overlay">
+            <div className="confirm-box flex flex-col">
+                <div className="message-section flex flex-col">
+                    {type === "warning" && <DangerIcon />}
+                    <p>{message}</p>
+                </div>
+                <div className="button-section flex">
+                    {isConfirmation && <button className={"button button-box"} onClick={onCancel}>Cancel</button>}
+                    <button className={`button button-box ${isConfirmation ? 'selected' : ''}`} onClick={onConfirm}>Confirm</button>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default ConfirmBox
