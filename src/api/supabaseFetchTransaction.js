bb353090493ffef989e08bdfd156b8880ad10b22
Ahmad Ghozali <ahmadgozali4040@gmail.com>
2025-09-08 23:50:39 +0700

    chore(api): adding transaction date args

--- src/api/supabaseFetchTransaction.js
+++ src/api/supabaseFetchTransaction.js
@@ -1,10 +1,18 @@
 import supabase from "./supabaseClient"
 
-const supabaseFetchTransaction = async () => {
-    const { data, error } = await supabase
-        .from('boncosku_users_transactions')
-        .select('*')
-    return {data, error}
+const supabaseFetchTransaction = async (transaction_date) => {
+    if (!transaction_date) {
+        const { data, error } = await supabase
+            .from('boncosku_users_transactions')
+            .select('*')
+            return {data, error}
+    } else {
+        const { data, error } = await supabase
+            .from('boncosku_users_transactions')
+            .select('*')
+            .gt('transaction_date', transaction_date)
+            return {data, error}
+    }
 }
 
 export default supabaseFetchTransaction
