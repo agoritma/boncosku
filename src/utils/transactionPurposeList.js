6c85d1f1c69407f06918919e4b702d237edd4ed2
Ahmad Ghozali <ahmadgozali4040@gmail.com>
2025-09-12 00:15:18 +0700

    add(transaction-purpose-list): adding list of transaction purpose

---
+++ src/utils/transactionPurposeList.js
@@ -0,0 +1,19 @@
+const transactionPuposeList = () => {
+    const list = [
+        {key:"Housing",label:"Housing",color:"#A8D5FF"},
+        {key:"Food & Drinks",label:"Food & Drinks",color:"#FFD6A5"},
+        {key:"Groceries",label:"Groceries",color:"#C8F0A8"},
+        {key:"Transport",label:"Transport",color:"#BEE3DB"},
+        {key:"Utilities",label:"Utilities",color:"#FFF3B0"},
+        {key:"Healthcare",label:"Healthcare",color:"#F7C6D6"},
+        {key:"Subscriptions",label:"Subscriptions",color:"#E5D3FF"},
+        {key:"Entertainment",label:"Entertainment",color:"#FFCBC1"},
+        {key:"Taxes & Debt",label:"Taxes & Debt",color:"#C8F1E6",},
+        {key:"Transfers",label:"Transfers",color:"#F0E6D6",},
+        {key:"-",label:"Uncategorized",color:"#BDBDBD"},
+    ]
+
+    return list
+}
+
+export default transactionPuposeList
